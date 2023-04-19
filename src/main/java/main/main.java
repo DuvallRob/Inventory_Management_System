c4982b10aac1b14e34908c9c58ff6a48b713e67f
Duvall Roberts <51434602+DuvallRob@users.noreply.github.com>
2023-04-19 13:53:13 -0400

    Update main.java

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,7 +1,6 @@
 package main;
 
 /**
- *
  * @author
  * Duvall Roberts
  * drob801@wgu.edu
@@ -15,19 +14,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ *
+ * The Inventory Management program is a software that manages
+ * an inventory of parts and products consisting of parts.
+ * 
+ * 
+ */
+
 /**Class that creates main screen of app*/
 public class main extends Application {
+    /**
+     *
+     * The start method that creates FXML stage and loads initial scene.
+     * 
+     * @param stage 
+     * @throws Exception
+     */
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/main-screen.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/main-screen.fxml"));//Location of Main Screen FXML document
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    /**@param args command line arguments*/
+    /**
+     * Main method is starting point of application.
+     * 
+     * Main method launches application.
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
         launch(args);
     }
