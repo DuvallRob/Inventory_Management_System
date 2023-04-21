7f40699f24b0a43baa7b42632d0ea664d093d8c0
Duvall Roberts <51434602+DuvallRob@users.noreply.github.com>
2023-04-21 13:37:20 -0400

    Update ModifyProductController.java

--- src/main/java/controller/ModifyProductController.java
+++ src/main/java/controller/ModifyProductController.java
@@ -30,75 +30,169 @@ import static model.Inventory.*;
 
 /**ModifyProductController class for modify-product.fxml*/
 public class ModifyProductController implements Initializable{
+    /**
+     * The product to be modified.
+     */
     Product productSelected;
 
+    /**
+     * The list of parts associated with the product.
+     */
     ObservableList<Part> partModProd = FXCollections.observableArrayList();
+
+    /**
+     * The index of the selected product.
+     */
     int indexSelected;
 
+    /**
+     * The text field for entering the ID of the product to be added.
+     */
     @FXML
     private TextField IDProductAddTextField;
 
+    /**
+     * The text field for entering the name of the product to be added.
+     */
     @FXML
     private TextField NameProductAddTextField;
 
+    /**
+     * The text field for entering the inventory of the product to be added.
+     */
     @FXML
     private TextField InventoryProductAddTextField;
 
+    /**
+     * The text field for entering the price of the product to be added.
+     */
     @FXML
     private TextField PriceProductAddTextField;
 
+    /**
+     * The text field for entering the maximum number of products that can be added.
+     */
     @FXML
     private TextField MaxProductAddTextField;
 
+    /**
+     * The text field for entering the minimum number of products that must be added.
+     */
     @FXML
     private TextField MinProductAddTextField;
 
+    /**
+     * The table view for displaying the parts to be modified.
+     */
     @FXML
     private TableView<Part> tableModifyPart;
 
+    /**
+     * The column for displaying the ID of the parts to be modified.
+     */
     @FXML
     private TableColumn<Part, Integer> partIdMod;
 
+    /**
+     * The column for displaying the name of the parts to be modified.
+     */
     @FXML
     private TableColumn<Part, String> partNameMod;
 
+    /**
+     * The column for displaying the inventory of the parts to be modified.
+     */
     @FXML
     private TableColumn<Part, Integer> partInventoryMod;
 
+    /**
+     * The column for displaying the price of the parts to be modified.
+     */
     @FXML
     private TableColumn<Part, Double> partPriceMod;
 
+    /**
+     * The table view for displaying the associated parts.
+     */
     @FXML
     private TableView<Part> tableModifyAssociatedPart;
 
+    /**
+     * The TableColumn class is used to represent a column in a table view of parts.
+     * This class contains the properties and methods necessary to display the associated part information.
+     *
+     * @param <Part> The type of the associated part.
+     */
     @FXML
     private TableColumn<Part, Integer> associatedPartIdMod;
 
+    /**
+     * The TableColumn class is used to represent a column in a table view of parts.
+     * This class contains the properties and methods necessary to display the associated part information.
+     *
+     * @param <Part> The type of the associated part.
+     */
     @FXML
     private TableColumn<Part, String> associatedPartNameMod;
 
+    /**
+     * The TableColumn class is used to represent a column in a table view of parts.
+     * This class contains the properties and methods necessary to display the associated part information.
+     *
+     * @param <Part> The type of the associated part.
+     */
     @FXML
     private TableColumn<Part, Integer> associatedPartInventoryMod;
 
+    /**
+     * The TableColumn class is used to represent a column in a table view of parts.
+     * This class contains the properties and methods necessary to display the associated part information.
+     *
+     * @param <Part> The type of the associated part.
+     */
     @FXML
     private TableColumn<Part, Double> associatedPartPriceMod;
 
+    /**
+     * The TextField class is used to represent a text field in the modify product page.
+     * This class contains the properties and methods necessary to display and manipulate the text field.
+     */
     @FXML
     private TextField modifyProductSearch;
 
+    /**
+     * The Button class is used to represent a button in the modify product page.
+     * This class contains the properties and methods necessary to display and manipulate the button.
+     */
     @FXML
     private Button addModifyProductPage;
 
+    /**
+     * The Button class is used to represent a button in the modify product page.
+     * This class contains the properties and methods necessary to display and manipulate the button.
+     */
     @FXML
     private Button removeModifyProductPage;
 
+    /**
+     * The Button class is used to represent a button in the modify product page.
+     * This class contains the properties and methods necessary to display and manipulate the button.
+     */
     @FXML
     private Button saveModifyProductPage;
 
+    /**
+     * The Button class is used to represent a button in the modify product page.
+     * This class contains the properties and methods necessary to display and manipulate the button.
+     */
     @FXML
     private Button cancelModifyProductPage;
-
-    /**Takes user back to Main Page*/
+    
+    /**
+     * This method handles the click event of the "Cancel" button on the "Modify Product" page and takes the user back to the main page.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
     @FXML
     void onClickCancelModifyProductPageButton() throws IOException {
 
@@ -109,11 +203,19 @@ public class ModifyProductController implements Initializable{
 
     }
 
-    /**Updates current part*/
+    /**
+     * This method updates the table of parts.
+     */
     public void updateTablePart() {
         tableModifyPart.setItems(getAllParts());
     }
-    /**Initialize controllers and populates the part and product tables.*/
+
+    /**
+     * This method initializes the controllers and populates the part and product tables.
+     *
+     * @param url the URL of the FXML file.
+     * @param rb  the resource bundle associated with the FXML file.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
@@ -134,7 +236,12 @@ public class ModifyProductController implements Initializable{
 
     }
 
-    /**Function for setting product*/
+    /**
+     * Sets the selected product and its properties at the specified index.
+     *
+     * @param product the product to be set
+     * @param index the index where the product should be set
+     */
     public void setProduct(Product product, int index) {
         productSelected = product;
         indexSelected = index;
@@ -151,7 +258,11 @@ public class ModifyProductController implements Initializable{
         }
     }
 
-    /**Updates current part and adds as new Product*/
+    /**
+     * Updates the current product and adds it as a new product.
+     * @param actionEvent The event that triggers the save or modify product action.
+     * @throws IOException If an input or output exception occurred.
+     */
     @FXML
     public void saveModifyProductButton(ActionEvent actionEvent) throws IOException {
         try {
@@ -161,7 +272,7 @@ public class ModifyProductController implements Initializable{
             } else if (!(Double.class.isInstance(Double.parseDouble(PriceProductAddTextField.getText())))) {
                 Alert priceAlert = new Alert(Alert.AlertType.ERROR, "Inventory value needs to be number!");
                 priceAlert.showAndWait();
-        } else if (Integer.parseInt(MaxProductAddTextField.getText()) < Integer.parseInt(MinProductAddTextField.getText())){
+            } else if (Integer.parseInt(MaxProductAddTextField.getText()) < Integer.parseInt(MinProductAddTextField.getText())){
                 Alert minAlert = new Alert(Alert.AlertType.ERROR, "Minimum can't be greater than maximum!");
                 minAlert.showAndWait();
             } else if (Integer.parseInt(InventoryProductAddTextField.getText()) < Integer.parseInt(MinProductAddTextField.getText()) || Integer.parseInt(InventoryProductAddTextField.getText()) > Integer.parseInt(MaxProductAddTextField.getText())){
@@ -195,34 +306,55 @@ public class ModifyProductController implements Initializable{
         }
     }
 
-    /**Removes selected from associated table*/
+    /**
+     * Removes the selected part from the associated parts table in the Modify Product screen.
+     * If no part is selected, an error alert is displayed.
+     *
+     * @param actionEvent The action event generated by clicking the remove button.
+     */
     public void removeModifyProductButton(ActionEvent actionEvent) {
 
+        // Get the selected part
         Part part = tableModifyAssociatedPart.getSelectionModel().getSelectedItem();
 
+        // Display an error alert if no part is selected
         if (part == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setHeaderText("Part not selected!");
             alert.showAndWait();
         } else {
+            // Display a confirmation alert before removing the part
             Alert remAlert = new Alert(Alert.AlertType.CONFIRMATION, "Want to remove part?");
             Optional<ButtonType> answer = remAlert.showAndWait();
             if (answer.isPresent() && answer.get() == ButtonType.OK) {
-               partModProd.remove(part);
-               tableModifyAssociatedPart.setItems(partModProd);
+                // Remove the part from the associated parts list and update the table
+                partModProd.remove(part);
+                tableModifyAssociatedPart.setItems(partModProd);
             }
         }
     }
 
-    /**Adds to associated table*/
+    /**
+     * Adds the selected part to the associated parts table in the Modify Product screen.
+     *
+     * @param actionEvent The action event generated by clicking the add button.
+     */
     public void addModifyProductButton(ActionEvent actionEvent) {
+        // Get the selected part
         Part part = tableModifyPart.getSelectionModel().getSelectedItem();
+        // Add the part to the associated parts list and update the table
         productSelected.addAssociatedPart(part);
         partModProd = productSelected.getAllAssociatedParts();
     }
 
-    /**Searches for part*/
+    /**
+     * This method searches for a part by ID or name and modifies the table to display the found part or parts.
+     * If the search field is empty, a warning message is displayed and the table is reset to display all parts.
+     * If the search value is not a valid integer, the method searches for parts by name instead.
+     *
+     * @param actionEvent The ActionEvent that triggers the searchModifyProductIDName method.
+     */
     public void searchModifyProductIDName(ActionEvent actionEvent) {
         String partSearched = modifyProductSearch.getText();
 
