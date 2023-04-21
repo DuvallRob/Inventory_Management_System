02318f7a93fe3d7c2a2b774594497eecf41def75
Duvall Roberts <51434602+DuvallRob@users.noreply.github.com>
2023-04-21 12:41:11 -0400

    Update InHouse.java

--- src/main/java/model/InHouse.java
+++ src/main/java/model/InHouse.java
@@ -7,22 +7,40 @@ package model;
  * drob801@wgu.edu
  * Student ID: 007792396
  */
-
-/**InHouse class that extends from Part class*/
 public class InHouse extends Part {
+
     private int machineId;
 
+    /**
+     * Constructs an InHouse object with specified id, name, price, stock, min, max, and machine ID.
+     *
+     * @param id the ID of the InHouse part.
+     * @param name the name of the InHouse part.
+     * @param price the price of the InHouse part.
+     * @param stock the inventory level of the InHouse part.
+     * @param min the minimum inventory level of the InHouse part.
+     * @param max the maximum inventory level of the InHouse part.
+     * @param machineId the machine ID of the InHouse part.
+     */
     public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
         this.machineId = machineId;
     }
 
-    /**@param machineId to set machineId*/
+    /**
+     * Sets the machine ID of the InHouse part to the specified machine ID.
+     *
+     * @param machineId the machine ID of the InHouse part.
+     */
     public void setMachineId(int machineId) {
         this.machineId = machineId;
     }
 
-    /**@return machineId*/
+    /**
+     * Returns the machine ID of the InHouse part.
+     *
+     * @return the machine ID of the InHouse part.
+     */
     public int getMachineId() {
         return machineId;
     }
